0784a2a024a407d66068313d7bedf7bada41ee99
Ana Pacheco <kornetera@gmail.com>
2023-03-04 13:08:23 +0000

    feat to do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,45 @@
-import React from "react";
-
+import React, {useState} from "react";
 
 
 const Home = () => {
-	return (
-		<div className="text-center">
-		
-	
-		</div>
-	);
+  const [input, setInput] = useState(""); //para controlar el cambio del input
+  const [tareas, setTareas] = useState([]); //para ir creando las tareas y meterlas en un array
+
+  return (
+    <div className="container">
+      <h1>To-Do List</h1>
+      <ul>
+        <li>
+        
+          <input
+            type="text"
+            onChange={(e) => setInput(e.target.value)} //cuando cambia el evento, lo almacena en setInput
+            value={input} // le decimos que el valor es el mismo que recibe el input
+            onKeyPress={(e) => //cuando se pulse tecla
+              e.key === "Enter" ? setTareas(tareas.concat(input)) : null //ternario para indicar que si la tecla es pulsada es enter, meta el valor de la misma en un array o "null"
+            }
+            placeholder="Add your to-do"
+          ></input>
+        </li>
+        {tareas.map((tarea, index) => ( //recorre el array de tareas para pintar cada una dentro de un li
+          <li>
+            {tarea} 
+            <i
+              className="fa-regular fa-trash-can"
+              onClick={() =>
+                setTareas(
+                  tareas.filter((tarea, currentIndex) => index != currentIndex)
+                )
+              } // mete el valor de la tarea en un li, añade el icono y al pulsar sobre él, se elimina la tarea pulsada con currentIndex
+
+            ></i>
+          </li>
+        ))}
+      </ul>
+
+      <div>{tareas.length} tareas</div> 
+    </div>
+  );
 };
 
 export default Home;
